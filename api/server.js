52011f518e6de195002b948ff799019ef08ffcea
Hector Cryo <sonic200887@gmail.com>
2024-05-26 10:38:30 +0300

    Crated and connected the database containing tables for users, products, orders and the cart

--- api/server.js
+++ api/server.js
@@ -13,12 +13,25 @@ const router = require('express').Router();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
     secret: 'nfdjd8vef(3fjv',
     resave: false,
     saveUninitialized: true,
     cookie: {secure: false, httpOnly: false, maxAge: 3600000, sameSite: 'strict'}
 })); 
+const pool = new Pool({
+    user: "hectorcryo",
+    host: "localhost",
+    database: "eCommerce",
+    password: "604b12bd9F!",
+    port: 5432
+});
+
+console.log(pool.query('SELECT * FROM users', (err, res) => {
+    console.log(err, res);
+}));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
